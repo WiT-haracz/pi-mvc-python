bd29f7f5db416c3647919175d925873e7c3e09a4
Paweł Haracz <14162492+PawelHaracz@users.noreply.github.com>
2024-01-13 20:43:31 +0000

    feat: Adjust python

--- project/controllers/printer.py
+++ project/controllers/printer.py
@@ -5,7 +5,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
-
 class CreateForm(FlaskForm):
     text = StringField('name', validators=[DataRequired()])
 
